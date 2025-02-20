ed28fac74bd48780c48b354ad58e40f5c818e827
Leonardo Dentz <leonardo.dentz@mycon.com.br>
2025-02-20 18:46:58 -0300

    chore: comment signinredirect

--- build/src/auth-context.js
+++ build/src/auth-context.js
@@ -295,12 +295,7 @@ var AuthProvider = function (_a) {
         return {
             signIn: function (args) { return __awaiter(void 0, void 0, void 0, function () {
                 return __generator(this, function (_a) {
-                    switch (_a.label) {
-                        case 0: return [4 /*yield*/, userManager.signinRedirect(args)];
-                        case 1:
-                            _a.sent();
-                            return [2 /*return*/];
-                    }
+                    return [2 /*return*/];
                 });
             }); },
             signInPopup: function () { return __awaiter(void 0, void 0, void 0, function () {
